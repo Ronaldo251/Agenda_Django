a4b53009f4bcd5ef4fffe6b821bbe8fe4cec417e
Ronaldo251 <naldoforrf@gmail.com>
2022-08-27 16:25:15 -0300

    Adicionando página de Eventos e filtros de busca

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo','data_evento','data_criacao')
-    list_filter = ('usuario','titulo',)
+    list_filter = ('usuario','titulo','data_evento',)
 
 
 admin.site.register(Evento, EventoAdmin)
